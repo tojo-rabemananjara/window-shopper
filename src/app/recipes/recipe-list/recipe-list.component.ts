d82f8258731fb9e81a07a60c30f4d79706c2c0aa
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-10-29 23:01:01 -0400

    populated header, recipes, recipe-detail, recipe-list, and shopping-list html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.marthastewart.com/styles/wmax-750/d64/spaghetti-shrimp-kale-burst-tomatoes-3opjx3r7kne9vih450ih3t-0421/spaghetti-shrimp-kale-burst-tomatoes-3opjx3r7kne9vih450ih3t-0421_horiz.jpg')
+  ];
 
   constructor() { }
 
